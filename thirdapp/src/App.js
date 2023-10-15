e4e700d791f52be76a06dba2f56813378e301f05
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2023-10-15 17:18:24 +0200

    thirdapp commit

---
+++ thirdapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App"> 
+      This is the starting code for "Your first component" ungraded lab 
+      <Heading /> 
+    </div>
+  );
+}
+
+function Heading() { 
+  return ( 
+    <h1>This is an h1 heading.</h1> 
+  ) 
+} 
+
+export default App;
